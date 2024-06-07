0a5603daa94c0fa94f1a191b3a8abecf22c914dd
Ehijie Omijie <ominoehijie@gmail.com>
2024-06-07 05:07:47 +0100

    creus

---
+++ projects/scripts/creus.js
@@ -0,0 +1,17 @@
+const year = document.querySelector('#currentyear');
+const today = new Date();
+year.innerHTML = today.getFullYear();
+
+const lastModified = document.querySelector('#lastModified');
+lastModified.innerHTML = (document.lastModified);
+
+// Footer pieces ^^^^
+// Hamburger Menu vvvv
+
+const hamButton = document.querySelector("#hamburger");
+const ul = document.querySelector("ul");
+
+hamButton.addEventListener("click", () => {
+    ul.classList.toggle("open-ul");
+    hamButton.classList.toggle("open");
+});
