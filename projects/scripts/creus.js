743f4ad5258169267f0a2c9282a63d973467dfb2
Ehijie Omijie <ominoehijie@gmail.com>
2024-06-09 05:09:27 +0100

    js lazy loader

--- projects/scripts/creus.js
+++ projects/scripts/creus.js
@@ -18,3 +18,15 @@ document.addEventListener('DOMContentLoaded', function() {
         hamburger.classList.toggle('open');
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyImages = document.querySelectorAll("img[data-src]");
+    
+    lazyImages.forEach(function(img) {
+      img.setAttribute("src", img.getAttribute("data-src"));
+      img.onload = function() {
+        img.removeAttribute("data-src");
+      };
+    });
+  });
