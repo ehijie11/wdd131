fbe9399278442f93a84bf9e309612de1b5e21fd5
Ehijie Omijie <ominoehijie@gmail.com>
2024-06-09 06:09:44 +0100

    a

--- projects/scripts/creus.js
+++ projects/scripts/creus.js
@@ -20,13 +20,3 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyImages = document.querySelectorAll("img[data-src]");
-    
-    lazyImages.forEach(function(img) {
-      img.setAttribute("src", img.getAttribute("data-src"));
-      img.onload = function() {
-        img.removeAttribute("data-src");
-      };
-    });
-  });
